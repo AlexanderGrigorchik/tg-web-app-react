3a6a104f4d76d2bd57b961120267b5b4bb60e2d9
Grigorchik Alexandr <grigorchik.alexandr@gmail.com>
2024-08-06 14:56:16 +0300

    Route 2

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -6,14 +6,14 @@ const Form = () => {
         <div className={"form"}>
             <h3>Заполните заявку</h3>
             <input className={'input'} type="text" placeholder={'Заголовок'}/>
-            <input className={'input'} type="text" placeholder={'Текст'}/>
 
             <select className={'select'}>
                 <option value={'cass'}>Конфиг для кассы</option>
                 <option value={'operator'}>Конфиг для оператора</option>
                 <option value={'printer'}>Конфиг для принтера</option>
             </select>
-            FORM
+
+            <input className={'input'} type="text" placeholder={'Текст'}/>
         </div>
     );
 };

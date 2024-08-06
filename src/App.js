b91e9e931e02aa659c70635f8cea2314f3f48dc6
Grigorchik Alexandr <grigorchik.alexandr@gmail.com>
2024-08-06 14:15:30 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./Header/Header";
+import {Route,Routes} from "react-router-dom";
+import Form from "./Form/Form";
+import Req from "./Req/Req";
 
 function App() {
     const{onToggleButton, tg} = useTelegram();
@@ -11,12 +13,13 @@ function App() {
         tg.ready();
     }, []);
 
-
-
   return (
     <div className="App">
         <Header />
-        <button onClick={onToggleButton}>Toogle</button>
+        <Routes>
+            <Route index element={<Req />}/>
+            <Route path={'/form'} element={<Form />}/>
+        </Routes>
     </div>
   );
 }
---
+++ src/Req/Req.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Req.css';
+
+const Req = () => {
+    return (
+        <div>
+            REQ
+        </div>
+    );
+};
+
+export default Req;

dafff04ed71c1684d07114d2fa12c019f11abd7b
Grigorchik Alexandr <grigorchik.alexandr@gmail.com>
2024-08-06 14:30:48 +0300

    Commit

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -3,7 +3,16 @@ import './Form.css';
 
 const Form = () => {
     return (
-        <div>
+        <div className={"form"}>
+            <h3>Заполните заявку</h3>
+            <input className={'input'} type="text" placeholder={'Заголовок'}/>
+            <input className={'input'} type="text" placeholder={'Текст'}/>
+
+            <select className={'select'}>
+                <option value={'cass'}>Конфиг для кассы</option>
+                <option value={'operator'}>Конфиг для оператора</option>
+                <option value={'printer'}>Конфиг для принтера</option>
+            </select>
             FORM
         </div>
     );
